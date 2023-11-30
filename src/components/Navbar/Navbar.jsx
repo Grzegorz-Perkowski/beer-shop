948c4bc171c4fcab08b7eeb585005b8866f6fa84
Grzegorz-Perkowski <58942682+Grzegorz-Perkowski@users.noreply.github.com>
2023-11-30 10:49:55 +0100

    Add quantity to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,8 +4,15 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Navbar() {
+  const cartItems = useSelector((state) => state.cart);
+
+  const totalQuantity = cartItems.reduce(
+    (accumulator, currentItem) => accumulator + currentItem.quantity,
+    0
+  );
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -19,6 +26,7 @@ function Navbar() {
           <Typography variant="h6" noWrap component="div">
             <Link to="/cart" style={{ textDecoration: "none", color: "white" }}>
               <ShoppingCartIcon />
+              {totalQuantity > 0 && <span>{totalQuantity}</span>}
             </Link>
           </Typography>
         </Toolbar>
